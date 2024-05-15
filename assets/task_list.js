b1d4945cdd5f26ccbee66b8b4975c85686ef5467
Gianni B. <gianni.bascetta@gmail.com>
2024-05-15 17:02:42 +0200

    upload

---
+++ assets/task_list.js
@@ -0,0 +1,22 @@
+//1.seleziona bottone in variabile btnAdd
+const btnAdd = document.querySelector("#insertDiv button");
+console.log(btnAdd);
+
+//+Crea array per lista task
+const taskList = [];
+
+////////////////////bottone è btnAdd
+//2.crea evento
+btnAdd.addEventListener("click", btnAddFunc);//click collega direttamente il bottone senza attributo "onclick", btnAddFunc collega alla funzione
+//3.cosa fa il click
+const btnAddFunc = function (event) {
+console.log("bottone cliccato");//fin qui tutto bene
+newLi = document.createElement("li")//Crea elemento lista
+newLi = newLi.innerText =//prendi da campo input */
+
+};
+
+ 
+
+
+
